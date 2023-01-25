035974ed442d3ce61c6566b4851c5053406acf86
MunaShabab <80338619+MunaShabab@users.noreply.github.com>
2023-01-25 13:04:38 -0500

    Add files via upload

---
+++ FlipACoin_MunaShabab/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace FlipACoin_MunaShabab
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.flipButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.headPic = new System.Windows.Forms.PictureBox();
+            this.tailPic = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.headPic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tailPic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // flipButton
+            // 
+            this.flipButton.Location = new System.Drawing.Point(27, 192);
+            this.flipButton.Name = "flipButton";
+            this.flipButton.Size = new System.Drawing.Size(67, 32);
+            this.flipButton.TabIndex = 0;
+            this.flipButton.Text = "Flip a Coin";
+            this.flipButton.UseVisualStyleBackColor = true;
+            this.flipButton.Click += new System.EventHandler(this.FlipButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Enabled = false;
+            this.clearButton.Location = new System.Drawing.Point(121, 192);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(67, 32);
+            this.clearButton.TabIndex = 1;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(213, 192);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(67, 32);
+            this.exitButton.TabIndex = 2;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // headPic
+            // 
+            this.headPic.Image = ((System.Drawing.Image)(resources.GetObject("headPic.Image")));
+            this.headPic.Location = new System.Drawing.Point(97, 37);
+            this.headPic.Name = "headPic";
+            this.headPic.Size = new System.Drawing.Size(114, 110);
+            this.headPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.headPic.TabIndex = 3;
+            this.headPic.TabStop = false;
+            this.headPic.Visible = false;
+            // 
+            // tailPic
+            // 
+            this.tailPic.Image = ((System.Drawing.Image)(resources.GetObject("tailPic.Image")));
+            this.tailPic.Location = new System.Drawing.Point(97, 37);
+            this.tailPic.Name = "tailPic";
+            this.tailPic.Size = new System.Drawing.Size(114, 110);
+            this.tailPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.tailPic.TabIndex = 4;
+            this.tailPic.TabStop = false;
+            this.tailPic.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(303, 248);
+            this.Controls.Add(this.tailPic);
+            this.Controls.Add(this.headPic);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.flipButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.headPic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tailPic)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button flipButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.PictureBox headPic;
+        private System.Windows.Forms.PictureBox tailPic;
+    }
+}
+
---
+++ FlipACoin_MunaShabab/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlipACoin_MunaShabab
+{
+    //Author: Muna Shabab
+    //ID:624191
+    //Date: 9-14-19
+    //Goal: to randomly flip a coin with switching button active status
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void FlipButton_Click(object sender, EventArgs e)
+        {
+            //variable to indicate which side is up
+            int sideUp;
+
+            //create a random object
+            Random rand = new Random();
+
+            //get a random integer between 0 and 1 
+            //0 means tails is up, 1 means heads is up
+            sideUp = rand.Next(2);
+
+            //display the side that is up
+            if (sideUp==0)
+            {
+                //display tails up
+                tailPic.Visible = true;
+                headPic.Visible = false;
+            }
+            else
+            {
+                //display heads up
+                headPic.Visible = true;
+                tailPic.Visible = false;
+            }
+
+            //enable the clear button
+            clearButton.Enabled = true;
+
+            //disable Flip a coin button
+            flipButton.Enabled = false;
+        }
+
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            //clear the pictures
+            headPic.Visible = false;
+            tailPic.Visible = false;
+
+            //enable the flip a coin button
+            flipButton.Enabled = true;
+
+            //disable the clear button
+            clearButton.Enabled = false;
+
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            //close the form
+            this.Close();
+        }
+
+    }
+}
